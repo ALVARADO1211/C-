dcf580c931f7c765d29a919edad51511817446c7
Alejandra Alvarado <tadizalvarado1211@gmail.com>
2026-04-06 17:43:43 -0600

    Create arreglo_1.cpp

---
+++ ejercicios_basicos_de_c++/arreglos/arreglo_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int arreglo[5]={1,2,3,4,5}; //IMPRIMIR NUMEROS
+
+int main()
+{
+    for(int x=0; x<5; x++)
+    {
+        cout<<arreglo[x]<<" | ";
+    }
+
+    return 0;
+}
